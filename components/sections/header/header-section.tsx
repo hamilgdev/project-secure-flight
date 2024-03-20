f13c5fb25e9bd9170367db97c776d18d5e78e1db
hamilgdev <hamilgdev@gmail.com>
2024-03-19 23:51:40 -0500

    Update header and home sections

--- components/sections/header/header-section.tsx
+++ components/sections/header/header-section.tsx
@@ -1,21 +1,28 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 export const HeaderSection = () => {
+  const pathname = usePathname()
+  const isHomePage = pathname === '/'
+
   return (
     <header
       className="min-h-[54px] flex items-center bg-transparent relative z-10">
       <div className="l-container">
         <div className="flex items-center justify-between">
-          <Link href="/">
+          <Link href="/auth/login">
             <Image src="/svgs/logo.svg" alt="Secure Flight" width={133} height={30} />
           </Link>
-
-          <Link
-            href="/dashboard"
-            className="uppercase bg-[var(--complementary)] text-white py-2 px-4 rounded-md text-base">
+          {isHomePage && (
+            <Link
+              href="/auth/login"
+              className="uppercase bg-[var(--complementary)] text-white py-2 px-4 rounded-md text-base">
               Comenzar
-          </Link>
+            </Link>
+          )}
         </div>
       </div>
     </header>
