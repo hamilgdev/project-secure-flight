b3c112daed19b3ae1502f0b5ac62b8776319decd
ARUS\juan.ruiz <juan.ruiz@arus.com.co>
2024-03-21 09:16:10 -0500

    ajuste consumo login

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -1,15 +1,26 @@
 import { PUBLIC_API } from '@/constants'
 
-export const postAuthLogin = async (nombreUsuario: string, clave: string) => {
+export const postAuthLogin = async (nombreUsuarioLogin: string, claveLogin: string) => {
   const url = `${PUBLIC_API}/autenticarUsuario`
-
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ nombreUsuario, clave })
-  })
-  const data = await response.json()
-  return data
+  console.log(url);
+  try {
+    console.log(JSON.stringify({ nombreUsuario: nombreUsuarioLogin, clave:  claveLogin}));
+    
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ nombreUsuario: nombreUsuarioLogin, clave:  claveLogin })
+    })
+    console.log(response);
+    const data = await response.json()
+    console.log(data);
+    return data
+  } catch (error) {
+    console.log(error);
+    
+  }
+  
+  
 }
